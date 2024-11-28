547a216668ca9671f48cd2e87f6db62d2fad541c
Jonas Langner <jonas.langner.kuz@fh-zwickau.de>
2024-11-28 17:08:10 +0100

    save positional embedding.

--- src/transformers/models/detr/modeling_detr.py
+++ src/transformers/models/detr/modeling_detr.py
@@ -18,6 +18,8 @@ import math
 from dataclasses import dataclass
 from typing import Dict, List, Optional, Tuple, Union
 
+import uuid
+
 import torch
 from torch import Tensor, nn
 
@@ -443,7 +445,7 @@ class DetrSinePositionEmbedding(nn.Module):
         pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
         pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
         pos = torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
-        print(pos)
+        torch.save(pos, "/content/position_embedding" + str(uuid.uuid4()) + ".pt")
         return pos
 
 
